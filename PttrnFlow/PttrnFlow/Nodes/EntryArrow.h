a55cbc69351d1a00e856706287f75dda4f8f04b5
John Saba <john.thomas.saba@gmail.com>
2013-06-09 10:29:33 -0500

    removed a lot of crap

---
+++ PttrnFlow/PttrnFlow/Nodes/EntryArrow.h
@@ -0,0 +1,13 @@
+//
+//  EntryArrow.h
+//  PttrnFlow
+//
+//  Created by John Saba on 5/27/13.
+//
+//
+
+#import "CellNode.h"
+
+@interface EntryArrow : CellNode
+
+@end
---
+++ PttrnFlow/PttrnFlow/Nodes/EntryArrow.m
@@ -0,0 +1,13 @@
+//
+//  EntryArrow.m
+//  PttrnFlow
+//
+//  Created by John Saba on 5/27/13.
+//
+//
+
+#import "EntryArrow.h"
+
+@implementation EntryArrow
+
+@end
