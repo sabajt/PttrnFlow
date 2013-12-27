baea175cb7d542012e99a99d64bd702e7356fe6e
John Saba <john.thomas.saba@gmail.com>
2013-12-26 20:17:58 -0600

    textured background image

--- PttrnFlow/PttrnFlow/Layers/BackgroundLayer.m
+++ PttrnFlow/PttrnFlow/Layers/BackgroundLayer.m
@@ -14,8 +14,7 @@
 + (BackgroundLayer *)backgroundLayer
 {
     BackgroundLayer *layer = [[self alloc] init];
-    CCSprite *backgroundImage = [CCSprite spriteWithSpriteFrameName:@"background_cream.png"];
-    backgroundImage.color = ccGRAY; // temporary while designing background texture
+    CCSprite *backgroundImage = [CCSprite spriteWithSpriteFrameName:@"background_checkers.png"];
     backgroundImage.position = ccp(layer.contentSize.width / 2, layer.contentSize.height / 2);
     [layer addChild:backgroundImage];
     return layer;
