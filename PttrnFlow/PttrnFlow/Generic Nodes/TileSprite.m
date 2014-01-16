4a6c13d52cc7e382181e3d23130021e2bed362b7
John Saba <john.thomas.saba@gmail.com>
2014-01-16 00:12:55 -0600

    introduce 'skip' to tile sprite

--- PttrnFlow/PttrnFlow/Generic Nodes/TileSprite.m
+++ PttrnFlow/PttrnFlow/Generic Nodes/TileSprite.m
@@ -13,22 +13,28 @@
 
 - (id)initWithTileFrameName:(NSString *)name repeatHorizonal:(int)repeatHorizontal repeatVertical:(int)repeatVertical
 {
+    return [self initWithTileFrameName:name repeatHorizonal:repeatHorizontal repeatVertical:repeatVertical skip:0];
+    
+}
+
+- (id)initWithTileFrameName:(NSString *)name repeatHorizonal:(int)repeatHorizontal repeatVertical:(int)repeatVertical skip:(NSInteger)skip
+{
     self = [super initWithSpriteFrameName:kClearRectUILayer];
     if (self)
     {
         CCSprite *tileSprite = [CCSprite spriteWithSpriteFrameName:name];
-        self.contentSize = CGSizeMake(tileSprite.contentSize.width * repeatHorizontal, tileSprite.contentSize.height * repeatVertical);
+        self.contentSize = CGSizeMake(tileSprite.contentSize.width * repeatHorizontal * (skip + 1), tileSprite.contentSize.height * repeatVertical * (skip + 1));
         
         for (int x = 0; x < repeatHorizontal; x++) {
             for (int y = 0; y < repeatVertical; y++) {
                 CCSprite *spr = [CCSprite spriteWithSpriteFrameName:name];
                 spr.anchorPoint = ccp(0, 0);
-                spr.position = ccp(x * spr.contentSize.width, y * spr.contentSize.height);
+                spr.position = ccp(x * spr.contentSize.width * (skip + 1), y * spr.contentSize.height * (skip + 1));
                 [self addChild:spr];
             }
         }
     }
-    return self;
-}
+    return self;}
+
 
 @end
